e1ece0a87037d3f90dd52ebf2d85e8f7a32c89cd
Luis <mikeylopez@gmail.com>
2015-10-27 13:34:14 -0400

    add step method and hook up to add step button

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,23 @@
 var Create = function(){};
 
-Create.prototype.addStep = function(form, position){
+var stepNameCount = 0;
+
+Create.prototype.addStep = function(insertionPoint, type){
+  var inputGroupClass = "input-group";
+  var inputGroupAddonClass = "input-group-addon";
+  var stepInputName = "step_name_" + (stepNameCount += 1);
+
   var inputGroup = document.createElement("div");
-   
+  var inputGroupAddon = document.createElement("div");
+  var stepInput = document.createElement("input");
+
+  inputGroup.className = inputGroupClass;
+  inputGroupAddon.className = inputGroupAddonClass;
+  inputGroupAddon.innerHTML = type + " :";
+  stepInput.className = "form-control";
+  stepInput.name = stepInputName;
+
+  $(inputGroup).append([inputGroupAddon, stepInput]);
+  $(insertionPoint).append(inputGroup);
+
 }
