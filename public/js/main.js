5ec39769a5633951523be81acb16de1db3babdae
Luis <mikeylopez@gmail.com>
2015-10-27 15:00:19 -0400

    remove step functionality

--- public/js/main.js
+++ public/js/main.js
@@ -10,14 +10,16 @@ Create.prototype.addStep = function(insertionPoint, type){
   var inputGroup = document.createElement("div");
   var inputGroupAddon = document.createElement("div");
   var stepInput = document.createElement("input");
+  var stepRemove = document.createElement("span");
 
   inputGroup.className = inputGroupClass;
   inputGroupAddon.className = inputGroupAddonClass;
+  stepRemove.className = "glyphicon glyphicon-remove";
   inputGroupAddon.innerHTML = type + " :";
   stepInput.className = "form-control";
   stepInput.name = stepInputName;
 
-  $(inputGroup).append([inputGroupAddon, stepInput]);
+  $(inputGroup).append([inputGroupAddon, stepInput, stepRemove]);
   $(insertionPoint).append(inputGroup);
 
 }
