f82540536425cb2b2128ab6cbf1b867dd2379241
Luis <mikeylopez@gmail.com>
2016-01-11 16:30:52 -0500

    updated db with steps table with foreign key on scenarios steps

--- lib/sql/core.sql
+++ lib/sql/core.sql
@@ -1,58 +1,105 @@
--- ---
--- Globals
--- ---
+-- MySQL dump 10.13  Distrib 5.6.27, for osx10.11 (x86_64)
+--
+-- Host: localhost    Database: pepino
+-- ------------------------------------------------------
+-- Server version	5.6.27
 
--- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
--- SET FOREIGN_KEY_CHECKS=0;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- ---
--- Table 'scenarios'
--- 
--- ---
+--
+-- Table structure for table `scenarios`
+--
 
 DROP TABLE IF EXISTS `scenarios`;
-		
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `scenarios` (
-  `id` INTEGER NOT NULL AUTO_INCREMENT,
-  `scenario_name` VARCHAR(255) NOT NULL,
-  `steps` MEDIUMTEXT,
-  `author` TINYINT NOT NULL,
-  `automated` TINYINT NOT NULL,
-  PRIMARY KEY (`id`)
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `scenario_name` varchar(255) NOT NULL,
+  `author` tinyint(4) NOT NULL,
+  `automated` tinyint(4) NOT NULL,
+  `steps` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `author` (`author`),
+  KEY `fk_steps_id` (`steps`),
+  CONSTRAINT `fk_steps_id` FOREIGN KEY (`steps`) REFERENCES `steps` (`id`),
+  CONSTRAINT `scenarios_ibfk_1` FOREIGN KEY (`author`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ---
--- Table 'users'
--- 
--- ---
+--
+-- Dumping data for table `scenarios`
+--
+
+LOCK TABLES `scenarios` WRITE;
+/*!40000 ALTER TABLE `scenarios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `scenarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `steps`
+--
+
+DROP TABLE IF EXISTS `steps`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `steps` (
+  `type` varchar(10) NOT NULL,
+  `step` varchar(255) NOT NULL,
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `steps`
+--
+
+LOCK TABLES `steps` WRITE;
+/*!40000 ALTER TABLE `steps` DISABLE KEYS */;
+/*!40000 ALTER TABLE `steps` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
 
 DROP TABLE IF EXISTS `users`;
-		
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `users` (
-  `id` TINYINT NOT NULL AUTO_INCREMENT,
-  `user_name` VARCHAR(16) NOT NULL,
-  `email` VARCHAR(50) NOT NULL,
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(16) NOT NULL,
+  `email` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ---
--- Foreign Keys 
--- ---
+--
+-- Dumping data for table `users`
+--
 
-ALTER TABLE `scenarios` ADD FOREIGN KEY (author) REFERENCES `users` (`id`);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- ---
--- Table Properties
--- ---
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- ALTER TABLE `scenarios` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `users` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- ---
--- Test Data
--- ---
-
--- INSERT INTO `scenarios` (`id`,`scenario_name`,`steps`,`author`,`automated`) VALUES
--- ('','','','','');
--- INSERT INTO `users` (`id`,`user_name`,`email`) VALUES
--- ('','','');
+-- Dump completed on 2016-01-11 16:30:19
