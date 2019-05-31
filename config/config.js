aa32ab474804380d1a36ff9a6bab29b63969e349
Luis <1561914+si-mikey@users.noreply.github.com>
2019-05-31 14:10:02 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,13 +1,6 @@
 var config = require('yamljs').load(__dirname + '/config.yml');
 
-function env_config(config){
-  if(process.env.ENV === 'prod'){
-    return config.production;
-  }else{
-    return config.development;
-  } 
-}
-
-module.exports = env_config(config);
+const env = process.env.ENV;
+module.exports = (env === 'prod') ? config.production : config.development; 
 
 
